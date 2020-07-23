0b14338da7c3dacf56dd49e79cac613f72c128b0
jdj2261 <wlseowhd12@naver.com>
2020-07-24 00:42:23 +0900

    modify setting.sh

--- setting.sh
+++ setting.sh
@@ -1,1 +1,2 @@
-cd /opt && sudo ln -sfn ~/joystick-res joystick
+cd /opt && ln -s /home/pi/joystick-res joystick
+echo "lxterminal -e /opt/joystick/joystick.sh" >> /etc/xdg/lxsession/LXDE-pi/autostart
