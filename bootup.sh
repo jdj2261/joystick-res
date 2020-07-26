b04ff45a6eccc946964ae7194af07b4dafe4708f
jdj2261 <wlseowhd12@naver.com>
2020-07-26 10:19:42 +0900

    modify bootup.sh

--- bootup.sh
+++ bootup.sh
@@ -1,7 +1,8 @@
-bootup_file="/home/pi/joystick-res/bootup"
+bootup_file=/home/pi/joystick-res/bootup
 
-if[-f $bootup_file];then
-	echo"bootup File is exists."
-
-mkdir bootup && cd bootup && ln -sfn ~/joystick-serial .
-echo "make bootup file & set soft link"
+if [ -d $bootup_file ]; then
+	echo "bootup Directory already exists."
+else
+	mkdir bootup && cd bootup && ln -sfn ~/joystick-serial .
+	echo "make bootup file & set soft link"
+fi
