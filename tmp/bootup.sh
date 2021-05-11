2e335f297d439bf16c596f68453c2027b49ddfd3
jdj2261 <wlseowhd12@naver.com>
2021-05-11 03:32:58 +0100

    deleted tmp directory

--- tmp/bootup.sh
+++
@@ -1,8 +0,0 @@
-bootup_file=/home/pi/joystick-res/bootup
-
-if [ -d $bootup_file ]; then
-	echo "bootup Directory already exists."
-else
-	mkdir bootup && cd bootup && ln -sfn ~/joystick-serial .
-	echo "make bootup file & set soft link"
-fi
--- tmp/setting.sh
+++
@@ -1,8 +0,0 @@
-cd /opt && ln -s /home/pi/joystick-res joystick
-echo "lxterminal -e /opt/joystick/joystick.sh" >> /etc/xdg/lxsession/LXDE-pi/autostart
-echo "## My alias ##" >> ~/.bashrc 
-echo "alias vbash='vi ~/.bashrc'" >> ~/.bashrc
-echo "alias sbash='source ~/.bashrc'" >> ~/.bashrc
-echo "alias ll='ls -al'" >> ~/.bashrc
-source ~/.bashrc
-
